f74ea0555a8ab5189d89e098361c604524ebec5f
sexfat <bryant6831@gmail.com>
2021-03-17 15:43:50 +0800

    add webpack config.js

--- es6.js
+++ es6.js
@@ -1,11 +1,20 @@
 // jquery
-import $ from 'jquery';
+// import $ from 'jquery';
+import Vue from 'vue';
 
 const callthings =(some) =>{
    return some + 'do something' 
 }
 
-console.log(callthings('aa'));
+console.log(callthings('bb'));
 
 
-$('body').css('background-color' , 'red');
+ new Vue({
+    el : '#app',
+    data : {
+     message : 'hellow Vue'  
+    }
+ });
+
+
+// $('body').css('background-color' , '#eee');
